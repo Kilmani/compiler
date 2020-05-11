afd60122b908ccae77368d3af41694152747b143
Kilmani <maksimovvlad2010@gmail.com>
2020-05-11 19:43:04 +0300

    5
    Работоспособность парсера по отдельным частям( ID package и тд)
    Лицензии Грамматики

--- lexanalyz/lexanalyzator.py
+++ lexanalyz/lexanalyzator.py
@@ -2,19 +2,27 @@ import sys
 from antlr4 import *
 from lexanalyz.gen.LexerGo import LexerGo
 from lexanalyz.gen.ParserGo import ParserGo
-from lexanalyz.gen import ParserGoListener
+from lexanalyz.gen.ParserGoListener import ParserGoListener
 
 
-class ParserGoListener(ParserGoListener):
-    def enterHi(self, ctx):
-        print("Hello: %s" % ctx.ID())
-    def main(argv):
-        lexer = LexerGo(StdinStream())
-        stream = CommonTokenStream(lexer)
-        parser = ParserGo(stream)
-        tree = parser.hi()
-        printer = ParserGoListener()
-        walker = ParseTreeWalker()
-        walker.walk(printer, tree)
+class ParserGoListenerPrint(ParserGoListener):
+   def enterHi(self, ctx):
+      print("Hello: %s" % ctx.ID())
+
+def main():
+   f = open('test.go', 'r')
+   giveMeInput = f.read()
+   print("PRINT: ".format(giveMeInput))
+
+   i_stream = InputStream(giveMeInput)
+
+   lexer = LexerGo(i_stream)
+   t_stream = CommonTokenStream(lexer)
+   parser = ParserGo(t_stream)
+   tree = parser.element()
+   printer = ParserGoListenerPrint()
+   walker = ParseTreeWalker()
+   walker.walk(printer, tree)
+
 if __name__ == '__main__':
-    main(sys.argv)
+   main()
